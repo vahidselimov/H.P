0ad510942c9ca252b2257eca26dc57df9bd2b2dd
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-04-24 06:07:09 +0400

    Task

---
+++ H.P/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace H.P
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = Console.ReadLine();
+            Studdents studdents = new Studdents("Vahid Selimov", subject);
+            Studdents studdents1 = new Studdents("Vahid Selimov", subject);
+            Console.WriteLine(studdents.Getname(subject) + studdents.Count);
+            Console.WriteLine(studdents1.Getname(subject) +studdents1.Count);
+
+
+            ////burda Id yoxlanisini kecirmisem ki Id var ve her defe artir.
+
+            //Console.WriteLine(studdents.Id);
+            //Console.WriteLine(studdents1.Id);
+
+
+            studdents.getinfo();
+        }
+    }
+}
---
+++ H.P/Studdents.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H.P
+{
+    internal  class Studdents
+    {
+        private static int _id;
+        public  int Id { get; set; }
+        public string Fullname  { get; set; }
+        public string Subject { get; set; }
+        private static int _count=1000;
+        public  int Count;
+        public Studdents(string fullname,string subject)
+        {
+            _count++;
+            Count = _count;
+            _id++;
+            Id = _id;
+            Fullname = fullname;
+            Subject = subject;
+        }
+        public string Getname(string subject )
+        {
+            if (subject == null)
+            {
+                
+                return null;
+ 
+            }
+            
+            return char.ToUpper(subject[0]) + char.ToUpper(subject[1]).ToString();
+           
+
+        }
+
+        ////Elave olaraq Get info methodu yaradib butun deyerleri ekranda gostermek olar
+
+        public void getinfo()
+        {
+            Console.WriteLine($"subject:{Subject},Count{Count},Fullname:{Fullname},Id{Id}");
+        }
+    }
+}
